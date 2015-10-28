fbfd7e0511a77f0e92c3dc6a3f6b0452b746cb29
Ross Williams <r.b.williams94@gmail.com>
2015-10-28 02:16:33 +0000

    Premium Users added which can now create and reply to posts.

--- src/SocialNetwork.java
+++ src/SocialNetwork.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SocialNetwork {
     
@@ -7,9 +8,48 @@ public class SocialNetwork {
     
     public SocialNetwork(String name){
         this.setName(name);
-        
+        currentUsers = new ArrayList<User>();
     }
 
+    public void addUser(User user){
+        try {
+            if (user.registered()){
+                currentUsers.add(user);
+            } else {
+                throw new UserNotRegisteredException("User "+ "'" + user.getUsername() + "'" + " must first register to the social network.");
+            }
+        } catch (UserNotRegisteredException e) {
+            System.out.println(e);
+        }
+        
+        
+    }
+    
+    
+    public void printCurrentUsers(){
+        System.out.println("Printing Current Users(" + currentUsers.size() +")......");
+        for (User user : currentUsers){
+            System.out.println("Username: " + user.getUsername() + "  Location: " + user.getGeoLocation() 
+                                + "  Account Type: " + user.getAccountType() + "  Blocked: " + user.blocked());
+        }
+    }
+    
+    public void printPosts(){
+        System.out.println("Printing all posts(" + Post.getCount() + ").....");
+        
+        for (User user : currentUsers){
+            try {
+                user.printPosts();
+            } catch (NullPointerException e){
+                user.printPosts();
+            }
+            
+        }
+        
+        
+    }
+    
+    /************** Getters and setters *****************/
     String getName() {
         return name;
     }
--- src/User.java
+++ src/User.java
@@ -1,4 +1,61 @@
+import java.util.ArrayList;
 
 public class User {
 
+    private String username;
+    private double geoLocation;
+    private boolean registered;
+    protected AccountType accountType;
+    private boolean blocked;
+    
+    public User(String username, double geoLocation){
+        this.setUsername(username);
+        this.setGeoLocation(geoLocation);
+        this.registered = false;
+        this.blocked = false;
+        this.accountType = AccountType.USER;
+    }
+    
+
+    public ArrayList<Post> getPosts(){
+        return null;
+    }
+    
+   public void printPosts(){
+       System.out.println("No posts to show for " + getUsername() + ", probably because of their account type being " + getAccountType());
+   }
+    
+    public void register(){
+        registered = true;
+    }
+    
+    public boolean registered(){
+        return registered;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public double getGeoLocation() {
+        return geoLocation;
+    }
+
+    public void setGeoLocation(double geoLocation) {
+        this.geoLocation = geoLocation;
+    }
+    
+    public AccountType getAccountType(){
+        return accountType;
+    }
+    
+    public boolean blocked(){
+        return blocked;
+    }
+    
+    
 }
